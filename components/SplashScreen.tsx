6d0e170d0bc68904c6a916b50d2ac91e2b064c76
LEtMeDEv <79841@naver.com>
2023-11-22 19:17:29 +0900

    update splash screen

--- components/SplashScreen.tsx
+++ components/SplashScreen.tsx
@@ -4,45 +4,10 @@ import { useEffect, useState } from 'react'
 import Cookies from 'js-cookie'
 import { useTheme } from 'next-themes'
 
-const viewCountIncreasingTime = 2000
-const lineAppearingTime = 500
-const hFullDelay = viewCountIncreasingTime + lineAppearingTime
-const logoAppearingTime = 700
-// const transparentingDelay = 1500
-const disappearingDelay = 3200
-
-const screenDisappearingTime = 1000
-
-type TTotalViewsProps = {
-  totalViews: number
-}
-const TotalViews = ({ totalViews }: TTotalViewsProps) => {
-  const countOfNumberToShow = 20
-  const [increasingNumber, setIncreasingNumber] = useState(totalViews - countOfNumberToShow)
-  useEffect(() => {
-    let count = 0
-    const interval = setInterval(() => {
-      setIncreasingNumber((prev) => prev + 1)
-      count += 1
-      if (count === countOfNumberToShow) clearInterval(interval)
-    }, viewCountIncreasingTime / countOfNumberToShow)
-  }, [])
-
-  return (
-    <div>
-      <span>{`${increasingNumber}`}</span>
-      <span className="text- sm:text-6xl md:text-9xl">views</span>
-    </div>
-  )
-}
-
 export default function SplashScreen() {
   const [height, setHeight] = useState<string>('h-0')
   const [opacity, setOpacity] = useState<string>('opacity-100')
   const [toShow, setToShow] = useState<boolean>(false)
-  const [totalViews, setTotalViews] = useState(0)
-  const [lineOpacity, setLineOpacity] = useState('opacity-0')
-  const [lineWidth, setLineWidth] = useState('w-0')
 
   const { theme } = useTheme()
 
@@ -52,59 +17,45 @@ export default function SplashScreen() {
 
   useEffect(() => {
     // if (typeof Cookies.get('Splashed') != 'undefined') return
-    const fetchTotalViews = async () => {
-      const { views } = await (await fetch('/api/views')).json()
-      setTotalViews(views)
-      setToShow(true)
-    }
-    fetchTotalViews()
-    setTimeout(() => setHeight('h-full'), hFullDelay)
-    setTimeout(() => setOpacity('opacity-0'), disappearingDelay)
-    setTimeout(() => {
-      setLineWidth('w-full')
-      setLineOpacity('opacity-100')
-    }, viewCountIncreasingTime)
+    setToShow(true)
+    setTimeout(() => setHeight('h-full'), 500)
+    setTimeout(() => setOpacity('opacity-0'), 1500)
     setTimeout(() => {
       setCookie()
       setToShow(false)
-    }, disappearingDelay)
+    }, 2200)
   }, [])
 
   return toShow ? (
     <div>
       <div
-        className={`fixed left-0 top-0 flex h-full w-full items-center justify-center bg-white text-4xl text-black antialiased ${opacity} duration-${screenDisappearingTime} dark:bg-gray-950 dark:text-white sm:text-6xl md:text-9xl`}
+        className={`fixed left-0 top-0 flex h-full w-full items-center justify-center bg-white text-4xl text-black antialiased ${opacity} duration-1000 dark:bg-gray-950 dark:text-white sm:text-6xl md:text-9xl`}
       >
-        <div className="relative flex h-12 w-full items-center justify-center sm:h-20 md:h-40">
-          {/* <div
+        <div className="relative flex h-12 w-full items-center sm:h-20 md:h-40">
+          <div
             className={`${
               theme == 'dark' ? 'light-text-edge' : 'dark-text-edge'
             } dark:light-text-edge absolute z-10 flex h-full w-full items-center justify-center font-bold text-white dark:text-gray-950`}
           >
-          LEtMeDEv
-          </div> */}
-          <div
-            className={`absolute z-10 flex h-full w-full items-center justify-center font-bold text-gray-950  dark:text-white`}
-          >
-            <TotalViews totalViews={totalViews} />
+            LEtMeDEv
           </div>
           <div
-            className={`transition-height z-50 flex h-full w-[60%] flex-col items-center justify-center `}
+            className={`transition-height   z-50 flex h-full w-full flex-col items-center justify-center  `}
           >
             <div
               className={`${
                 theme == 'dark' ? 'light-line-shadow' : 'dark-line-shadow'
-              }  z-10 h-[1px] ${lineWidth} bg-white duration-500 ease-linear ${lineOpacity}`}
+              }  h-[1px] w-[60%] bg-gray-400`}
             />
             <div
-              className={`flex ${height} w-ful items-center justify-center overflow-hidden bg-white font-bold duration-${logoAppearingTime} ease-linear dark:bg-gray-950`}
+              className={`flex ${height} w-ful items-center justify-center overflow-hidden  font-bold duration-700 ease-linear`}
             >
               LEtMeDEv
             </div>
             <div
               className={`${
                 theme == 'dark' ? 'light-line-shadow' : 'dark-line-shadow'
-              }  z-10 h-[1px] ${lineWidth} bg-white duration-500 ease-linear ${lineOpacity}`}
+              }  h-[1px] w-[60%] bg-gray-400`}
             />
           </div>
         </div>
