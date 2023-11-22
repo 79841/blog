3e8d213d7816741d896b150d9b5ce3c89dddc8ac
LEtMeDEv <79841@naver.com>
2023-11-22 19:17:21 +0900

    update view count

--- app/_components/ViewCount.tsx
+++ app/_components/ViewCount.tsx
@@ -1,18 +1,24 @@
 'use client'
+
 import { useEffect, useState } from 'react'
 
-type Props = {
+type TProps = {
   slug: string
 }
-const MainPostPreview = ({ slug }: Props) => {
+
+const ViewCount = ({ slug }: TProps) => {
+  const title = slug.split('/').at(-1)
+
   const [viewCount, setViewCount] = useState(0)
+
   useEffect(() => {
     const fetchViews = async () => {
-      const data = await (await fetch(`/api/views/${slug.split('/').at(-1)}`)).json()
+      const data = await (await fetch(`/api/views/${title}`)).json()
       setViewCount(data != null ? data.views : 0)
     }
     fetchViews()
-  }, [])
-  return <div className="text-right text-gray-500 dark:text-gray-400">{`views: ${viewCount}`}</div>
+  })
+  return <div className="flex items-center justify-end">{`${viewCount} views`}</div>
 }
-export default MainPostPreview
+
+export default ViewCount
